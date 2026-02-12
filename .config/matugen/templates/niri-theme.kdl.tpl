fbcdd479e89fe69596bd376865993e089268d406
helpmeagain <57302703+helpmeagain@users.noreply.github.com>
2026-02-12 14:01:34 -0300

    feat: add noctalia shell

--- .config/matugen/templates/niri-theme.kdl.tpl
+++
@@ -1,6 +0,0 @@
-layout {
-    border {
-        active-color "{{colors.primary.default.hex}}"
-        inactive-color "{{colors.outline.default.hex}}"
-    }
-}
--- .config/waybar/scripts/launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-killall -9 waybar
-
-waybar &
-
-# waybar -c ~/.config/waybar/themes/minimal/config.jsonc -s ~/.config/waybar/themes/minimal/style.css
-
